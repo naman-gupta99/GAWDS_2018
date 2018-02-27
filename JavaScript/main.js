256a0a38de9a24a581757d8c0a69b2c63f1a34c3
war-turtle <kartikyadav71199@gmail.com>
2018-02-27 20:46:37 +0530

    main files added

---
+++ JavaScript/main.js
@@ -0,0 +1,62 @@
+$(function(){
+  var container = $('.container')
+  //     bdy = $('body'),
+  //     cnWidth = container.width(),
+  //     cnHeight = container.height(),
+  //     wWidth = window.innerWidth,
+  //     wHeight = window.innerHeight,
+  //     pos = container.position();
+  //     middleSide = wWidth/2 - cnWidth/2,
+  //     rightSide = wWidth - 2*cnWidth,
+  //     leftSide =  cnWidth,
+  //     topSide = "5%",
+  //     centerSide = (cnHeight + wHeight)/2 - cnHeight;
+  //     bottomSide = wHeight - 2*cnHeight;
+  //
+  // console.log(cnWidth,cnHeight,wWidth,wHeight,pos);
+
+  // TweenMax.set(container,{
+    // position:'fixed',
+    // top:0,
+    // left:0,
+    // bottom:0,
+    // right:0,
+    // margin:'auto',
+    // transform:'rotateY(-90deg) rotateX(-20deg)',
+    // transformStyle:"preserve-3d",
+    // width:'50px',
+    // height:'50px',
+    // transformOrigin:"center center 100%"
+  // })
+  // TweenMax.to('#plane',10,{
+  //   // rotation:"0"
+  //   rotationX:360,
+  //   transformOrigin:"100% 100%",
+  //   delay:12
+  // })
+
+
+});
+
+// (function give(){
+//   var wWidth = window.innerWidth;
+//   var wHeight = window.innerHeight;
+//   var position = $('.container').position();
+//   setTimeout(function(){
+//     console.log(position);
+//   },2000);
+// })();
+// var container = $('.container')[0];
+// var rect = container.getBoundingClientRect();
+// console.log(rect.top);
+// setInterval(function(){
+//   console.log(container.position.left)
+// },1000)
+
+// window.onresize = function(){
+// console.log(window.innerWidth);
+// }
+
+// var cont = $('.container')[0];
+// var rect = cont.getBoundingClientRect();
+// console.log(cont.clientHeight,cont.clientWidth,rect.top,rect.rightSide,rect.bottom,rect.left)
