68ee89288270b1b050fb4e7d92f9e4a0075b18d7
war-turtle <kartikyadav71199@gmail.com>
2018-03-31 22:52:57 +0530

    animation almost finished, ready for improvement

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -16,7 +16,7 @@ $(function(){
   //
   // console.log(cnWidth,cnHeight,wWidth,wHeight,pos);
 
-  // get the page to the top after every refresh
+  //get the page to the top after every refresh
   $(window).on('beforeunload', function() {
       $(window).scrollTop(0);
   });
@@ -133,34 +133,6 @@ $(function(){
 
   }
   windAnimation();
-
-  function wordAnimation(){
-    var words = $('.wording');
-
-    TweenLite.to(words,1,{
-      opacity:1,
-      y:"-=10px",
-      delay:5.5
-    })
-
-    var time = new TimelineMax({paused:true});
-
-    time
-      .to(words,1,{
-        opacity:0,
-        y:"-=20px"
-      });
-
-    var scene = new ScrollMagic.Scene({
-      triggerElement:bdy,
-      triggerHook:0,
-      duration:0.5*wHeight,
-      reverse:false
-    })
-    .setTween(time.play())
-    .addTo(controller);
-  }
-  wordAnimation();
 
   function planeAnimation(p1){
 
@@ -340,9 +312,13 @@ $(function(){
         autoAlpha:0
       },"-=5")
 
+      .to('#innerBackground',5,{
+        opacity:1
+      })
+
       .to(portalWhole,5,{
         scale:1
-      })
+      },"-=5")
 
       .staggerTo(backPath,2,{
         scale:1,
@@ -446,230 +422,435 @@ $(function(){
   }
   portalAnimation();
 
-  // function demo(){
-  //   var portal = $('.portal');
-  //
-  //   var time = new TimelineMax();
-  //
-  //   time
-  //     .to(portal,10,{
-  //       scale:10,
-  //       transformOrigin:"center center"
-  //     })
-  // }
-  // demo();
+  // animation for galaxy
+  function galaxyAnimation(){
 
-  // function emerge(){
-  //   var time = new TimelineMax({paused:true});
-  //
-  //   time
-  //     .to(wormhole,0.1,{
-  //       left:"80%",
-  //       top:"20%"
-  //     })
-  //     .to(wormhole,1,{
-  //       scale:1
-  //     })
-  //     .to([$("#lwing"),$("#rwing")],0.1,{
-  //       borderBottom:"50px solid green"
-  //     })
-  //     .to(plane,0.1,{
-  //       left:"80%",
-  //       top:"20%"
-  //     })
-  //     .to(plane,1,{
-  //       rotation:0,
-  //       scale:1
-  //     })
-  //     .to(wormhole,0.5,{
-  //       rotationY:90,
-  //       rotationX:-90,
-  //       autoAlpha:0,
-  //       scale:0,
-  //       onComplete:rest
-  //     });
-  //
-  //   var scene = new ScrollMagic.Scene({
-  //     triggerElement:bdy,
-  //     triggerHook:0,
-  //     duration:wHeight,
-  //     offset:3*wHeight
-  //   })
-  //   .setTween(time.play())
-  //   .addTo(controller);
-  // }
-  //
-  // function rest(){
-  //   var time = new TimelineMax({paused:true});
-  //
-  //   time
-  //     .to(plane,1,{
-  //     rotationX:10,
-  //     rotation:-10
-  //     })
-  //     .to(plane,5,{
-  //       left:"50%",
-  //       top:"50%",
-  //     })
-  //     .to(plane,1,{
-  //       rotationX:0,
-  //       rotation:0
-  //     })
-  //     .to(plane,10,{
-  //       delay:2,
-  //       scale:0.7,
-  //       top:"40%",
-  //       rotationX:-20
-  //     })
-  //     .to(plane,10,{
-  //       scale:0.5,
-  //       rotationX:0
-  //     })
-  //     .to(plane,10,{
-  //       delay:5,
-  //       left:"20%",
-  //       rotation:-360,
-  //       transformOrigin:"50% 100%"
-  //     })
-  //
-  //   var scene = new ScrollMagic.Scene({
-  //     triggerElement:bdy,
-  //     triggerHook:0,
-  //     duration:wHeight,
-  //     offset:4*wHeight
-  //   })
-  //   .setTween(time.play())
-  //   .addTo(controller);
-  // }
-  //
+    var time = new TimelineMax({paused:true});
 
-  // function nextToBack(){
-  //   console.log('kartik')
-  //   var time = new TimelineMax({paused:true});
-  //
-  //   time
-  //     .to(plane,1,{
-  //       top:"70%",
-  //       left:"10%"
-  //     })
-  //     .to([$("#lside"),$("#rside")],1,{
-  //       borderRight:"100px solid red"
-  //     })
-  //     .to(plane,5,{
-  //       rotation:360,
-  //       scale:1
-  //     })
-  //     .to(wormhole,5,{
-  //       scale:0,
-  //       autoAlpha:0
-  //     })
-  //     .to(plane,5,{
-  //       rotationX:-20,
-  //       rotationY:-20
-  //     })
-  //     .to(plane,10,{
-  //       left:"50%",
-  //       top:"50%"
-  //     })
-  //     .to(plane,5,{
-  //       rotationX:0,
-  //       rotationY:0
-  //     })
-  //     .to(plane,0.5,{
-  //       rotation:0
-  //     })
-  //     .to(plane,2,{
-  //       rotation:20
-  //     })
-  //     .to(plane,2,{
-  //       rotation:0
-  //     })
-  //     .to(plane,2,{
-  //       rotation:-20
-  //     })
-  //     .to(plane,2,{
-  //       rotation:0
-  //     })
-  //     .to(plane,2,{
-  //       rotation:20
-  //     })
-  //     .to(plane,2,{
-  //       rotation:0
-  //     })
-  //     .to(plane,2,{
-  //       rotation:-20
-  //     })
-  //     .to(plane,2,{
-  //       rotation:0
-  //     });
-  //
-  //   var scene = new ScrollMagic.Scene({
-  //     triggerElement:bdy,
-  //     triggerHook:0,
-  //     duration:wHeight,
-  //     offset:5.5*wHeight
-  //   })
-  //   .setTween(time.play())
-  //   .addTo(controller);
-  // }
-  //
-  // function graphicsAnimation(){
-  //   var illus = $(".illustrator");
-  //   var pts = $(".photoshop");
-  //   var draw = $(".draw");
-  //
-  //
-  //   var time = new TimelineMax({paused:true});
-  //
-  //   time
-  //     .to(illus,5,{
-  //       top:"50%",
-  //       yPercent:-100
-  //     })
-  //     .to(draw,5,{
-  //       left:"50%",
-  //       xPercent:-100
-  //     },"-=5")
-  //     .to(pts,5,{
-  //       left:"50%",
-  //       xPercent:100
-  //     },"-=5")
-  //     .to(illus,5,{
-  //       bezier:{
-  //         curviness:1,values:[
-  //           {xPercent:0,yPercent:-100},{xPercent:100,yPercent:0},{xPercent:0,yPercent:100},{xPercent:-100,yPercent:0},{xPercent:0,yPercent:-100}
-  //         ]
-  //     }
-  //     })
-  //     .to(draw,5,{
-  //       bezier:{
-  //         curviness:1,values:[
-  //           {xPercent:-100,yPercent:0},{xPercent:0,yPercent:-100},{xPercent:100,yPercent:0},{xPercent:0,yPercent:100},{xPercent:-100,yPercent:0}
-  //         ]
-  //       }
-  //     },"-=5")
-  //     .to(pts,5,{
-  //       bezier:{
-  //         curviness:1,values:[
-  //           {xPercent:100,yPercent:-0},{xPercent:0,yPercent:100},{xPercent:-100,yPercent:0},{xPercent:0,yPercent:-100},{xPercent:100,yPercent:0}
-  //         ]
-  //       }
-  //     },"-=5")
-  //     .to([illus,pts,draw],5,{
-  //       scale:0
-  //     },"-=5")
-  //     .to(wormhole,5,{
-  //       autoAlpha:1,
-  //       scale:1
-  //     },"-=5");
-  //
-  //     var scene = new ScrollMagic.Scene({
-  //       triggerElement:bdy,
-  //       triggerHook:0,
-  //       duration:2*wHeight
-  //     })
-  //     .setTween(time.play())
-  //     .addTo(controller);
-  // }
-  // graphicsAnimation();
+    time
+      .to('.galaxy',5,{
+        opacity:1
+      })
+      .to('.galaxy svg',5,{
+        opacity:1
+      },"-=2")
+      .to(".galaxy",10,{
+        scale:20
+      },"+=5")
+      .to(plane,10,{
+        scale:0.5,
+        rotationX:5
+      },"-=5")
+      .to(plane,10,{
+        scale:1,
+        rotationX:0,
+        left:"50%"
+      })
+
+
+      var scene = new ScrollMagic.Scene({
+        triggerElement:bdy,
+        triggerHook:0,
+        duration:3*wHeight,
+        offset:17*wHeight
+      })
+      .setTween(time.play())
+      .addTo(controller);
+
+  }
+  galaxyAnimation();
+
+  function rotatePlanet(){
+
+    TweenMax.to('.group',10,{
+      rotationX:20,
+      rotationY:20,
+      yoyo:true,
+      repeat:-1
+    });
+
+    var time = new TimelineMax({repeat:-1});
+
+    time
+
+      .to('.icon',10,{
+        rotation:360,
+        transformOrigin:"50% 50%",
+        ease: Power0.easeNone
+      })
+
+      .to('#illustrator',10,{
+        rotation:360,
+        transformOrigin:"50% 50%",
+        ease: Power0.easeNone
+      },"-=10")
+
+      .to('#draw',10,{
+        rotation:-360,
+        transformOrigin:"50% 50%",
+        ease: Power0.easeNone
+      },"-=10")
+
+      .to('#photoshop',10,{
+        rotation:720,
+        transformOrigin:"50% 50%",
+        ease: Power0.easeNone
+      },"-=10");
+  }
+  rotatePlanet();
+
+  function laptopAnimation(){
+
+    var topS = $('#sit')[0].getBoundingClientRect().top;
+    var leftS = $('#sit')[0].getBoundingClientRect().left;
+
+    var time = new TimelineMax({paused:true});
+
+    time
+      .to('.laptopContainer',5,{
+        autoAlpha:1
+      })
+      .to('.laptop',5,{
+        autoAlpha:1
+      })
+      .to(plane,5,{
+        top:topS,
+        left:leftS,
+        scale:0.5
+      })
+      .to(plane,5,{
+        rotation:0,
+        rotationX:20,
+        rotationY:"180_ccw",
+        scale:0.5,
+        transformOrigin:"center center"
+      },10)
+      .to(plane,2,{
+        rotationX:5
+      });
+
+    var scene = new ScrollMagic.Scene({
+      triggerElement:bdy,
+      triggerHook:0,
+      duration:3*wHeight,
+      offset:20*wHeight
+    })
+    .setTween(time.play())
+    .addTo(controller);
+  }
+  laptopAnimation();
+
+  function setPositionAbsolute(){
+
+    var time = new TimelineMax({paused:true});
+
+    time
+      .to('#background',10,{
+        position:'absolute'
+      })
+      .to('.wind',10,{
+        position:'absolute'
+      })
+      .to('.frontpoly',10,{
+        position:'absolute'
+      })
+      .to('.backpoly',10,{
+        position:'absolute'
+      })
+      .to('.fog',10,{
+        position:'absolute'
+      })
+      .to('.css',10,{
+        position:'absolute'
+      })
+      .to('.html',10,{
+        position:'absolute'
+      })
+      .to('.js',10,{
+        position:'absolute'
+      })
+      .to('.nodejs',10,{
+        position:'absolute'
+      })
+      .to('.django',10,{
+        position:'absolute'
+      })
+      .to('.sql',10,{
+        position:'absolute'
+      })
+      .to('.firebase',10,{
+        position:'absolute'
+      })
+      .to('.illustrator',10,{
+        position:'absolute'
+      })
+      .to('.draw',10,{
+        position:'absolute'
+      })
+      .to('.photoshop',10,{
+        position:'absolute'
+      })
+      .to('.portalWhole',10,{
+        position:'absolute'
+      })
+      .to('.whitedot',10,{
+        position:'absolute'
+      })
+      .to('.stream',10,{
+        position:'absolute'
+      })
+      .to('.galaxy',10,{
+        position:'absolute'
+      })
+      .to('.galaxy svg',10,{
+        position:'absolute'
+      })
+
+    var scene = new ScrollMagic.Scene({
+      triggerElement:bdy,
+      triggerHook:0,
+      duration:wHeight,
+      offset:21*wHeight
+    })
+    .setTween(time.play())
+    .addTo(controller);
+  }
+  // setPositionAbsolute();
+
+  function convertToCircle(){
+    var laptopContainer = $('.laptopContainer');
+    var laptop = $('.laptop');
+    var innerWidth = window.innerWidth;
+
+    var time = new TimelineMax({paused:true});
+
+    if(innerWidth <= '760'){
+
+      time
+        .to('.writtenContent',5,{
+          opacity:1
+        })
+        .to(plane,1,{
+          opacity:0
+        })
+        .to(laptopContainer,10,{
+          width:'40vw',
+          height:'40vw',
+          borderRadius: '40vw'
+        })
+
+        .to(laptop,10,{
+          scale:0.5
+        },"-=10")
+
+        .to(laptopContainer,10,{
+          top:"20%"
+        });
+    } else {
+      time
+        .to('.writtenContent',5,{
+          opacity:1
+        })
+        .to(plane,1,{
+          opacity:0
+        })
+        .to(laptopContainer,10,{
+          width:'40vw',
+          height:'40vw',
+          borderRadius: '40vw'
+        })
+        .to(laptop,10,{
+          scale:0.5
+        },"-=10")
+
+        .to(laptopContainer,10,{
+          left:"75%"
+        });
+    }
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement:bdy,
+        triggerHook:0,
+        duration:2*wHeight,
+        offset:23*wHeight
+      })
+      .setTween(time.play())
+      .addTo(controller);
+
+  }
+  convertToCircle();
+
+  function changeFixedToAbsolute(){
+    var w = 26*wHeight;
+
+    var scene1 = new ScrollMagic.Scene({
+      triggerElement:bdy,
+      triggerHook:0,
+      offset:26*wHeight
+    })
+    .setClassToggle('.laptopContainer', 'laptopClassToggle')
+    .addTo(controller);
+
+    var scene2 = new ScrollMagic.Scene({
+      triggerElement:bdy,
+      triggerHook:0,
+      offset:26*wHeight
+    })
+    .setClassToggle('.writtenContent', 'writtenContentClassToggle')
+    .addTo(controller);
+  }
+  changeFixedToAbsolute();
+
 });
+
+/*
+
+
+function end(){
+  $(window).scroll(function(){
+    if(pageYOffset >= 23*wHeight){
+      $('.laptopContainer').css('top',23*wHeight+'px');
+      $('.laptopContainer').css('position','absolute');
+
+      $('#background').css('top',23*wHeight+'px');
+      $('#background').css('position','absolute');
+
+      $('.wind').css('top',23*wHeight+'px');
+      $('.wind').css('position','absolute');
+
+      $('.wording').css('top',23*wHeight+'px');
+      $('.wording').css('position','absolute');
+
+      $('.frontpoly').css('top',23*wHeight+'px');
+      $('.frontpoly').css('position','absolute');
+
+      $('.backpoly').css('top',23*wHeight+'px');
+      $('.backpoly').css('position','absolute');
+
+      $('.fog').css('top',23*wHeight+'px');
+      $('.fog').css('position','absolute');
+
+      $('.css').css('top',23*wHeight+'px');
+      $('.css').css('position','absolute');
+
+      $('.html').css('top',23*wHeight+'px');
+      $('.html').css('position','absolute');
+
+      $('.js').css('top',23*wHeight+'px');
+      $('.js').css('position','absolute');
+
+      $('.nodejs').css('top',23*wHeight+'px');
+      $('.nodejs').css('position','absolute');
+
+      $('.django').css('top',23*wHeight+'px');
+      $('.django').css('position','absolute');
+
+      $('.sql').css('top',23*wHeight+'px');
+      $('.sql').css('position','absolute');
+
+      $('.firebase').css('top',23*wHeight+'px');
+      $('.firebase').css('position','absolute');
+
+      $('.illustrator').css('top',23*wHeight+'px');
+      $('.illustrator').css('position','absolute');
+
+      $('.draw').css('top',23*wHeight+'px');
+      $('.draw').css('position','absolute');
+
+      $('.photoshop').css('top',23*wHeight+'px');
+      $('.photoshop').css('position','absolute');
+
+      $('.portalWhole').css('top',23*wHeight+'px');
+      $('.portalWhole').css('position','absolute');
+
+      $('.whitedot').css('top',23*wHeight+'px');
+      $('.whitedot').css('position','absolute');
+
+      $('.stream').css('top',23*wHeight+'px');
+      $('.stream').css('position','absolute');
+
+      $('.galaxy').css('top',23*wHeight+'px');
+      $('.galaxy').css('position','absolute');
+
+      $('.galaxy svg').css('top',23*wHeight+'px');
+      $('.galaxy svg').css('position','absolute');
+
+      $('.laptopContainer').css('top',23*wHeight+'px');
+      $('.laptopContainer').css('position','absolute');
+
+    }else{
+      $('.laptopContainer').css('top','0');
+      $('.laptopContainer').css('position','fixed');
+
+      $('#background').css('top','0');
+      $('#background').css('position','fixed');
+
+      $('.wind').css('top','0');
+      $('.wind').css('position','fixed');
+
+      $('.wording').css('top','0');
+      $('.wording').css('position','fixed');
+
+      $('.frontpoly').css('top','0');
+      $('.frontpoly').css('position','fixed');
+
+      $('.backpoly').css('top','0');
+      $('.backpoly').css('position','fixed');
+
+      $('.fog').css('top','0');
+      $('.fog').css('position','fixed');
+
+      $('.css').css('top','0');
+      $('.css').css('position','fixed');
+
+      $('.html').css('top','0');
+      $('.html').css('position','fixed');
+
+      $('.js').css('top','0');
+      $('.js').css('position','fixed');
+
+      $('.nodejs').css('top','-10%');
+      $('.nodejs').css('position','fixed');
+
+      $('.django').css('top','-10%');
+      $('.django').css('position','fixed');
+
+      $('.sql').css('top','-10%');
+      $('.sql').css('position','fixed');
+
+      $('.firebase').css('top','-10%');
+      $('.firebase').css('position','fixed');
+
+      $('.illustrator').css('top','0');
+      $('.illustrator').css('position','fixed');
+
+      $('.draw').css('top','0');
+      $('.draw').css('position','fixed');
+
+      $('.photoshop').css('top','0');
+      $('.photoshop').css('position','fixed');
+
+      $('.portalWhole').css('top','0');
+      $('.portalWhole').css('position','fixed');
+
+      $('.whitedot').css('top','0');
+      $('.whitedot').css('position','fixed');
+
+      $('.stream').css('top','0');
+      $('.stream').css('position','fixed');
+
+      $('.galaxy').css('top','0');
+      $('.galaxy').css('position','fixed');
+
+      $('.galaxy svg').css('top','0');
+      $('.galaxy svg').css('position','fixed');
+
+      $('.laptopContainer').css('top','0');
+      $('.laptopContainer').css('position','fixed');
+    }
+  })
+
+
+}
+  end();
+*/
